e6225e87cdfb98d358b769ae30b685a84da79421
Robin Stark <robin.stark@visfleet.com>
2015-03-06 14:53:31 +1300

    join array opts together with spaces

--- libraries/default.rb
+++ libraries/default.rb
@@ -1,6 +1,8 @@
 def collectd_option(option)
   if option.kind_of? String
     "\"#{option}\""
+  elsif option.kind_of? Array
+    option.map { |o| collectd_option(o) }.join(" ")
   else
     option
   end
